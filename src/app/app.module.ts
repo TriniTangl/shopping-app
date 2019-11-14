6d685f6aec8298aae8a1f656a02a38844bab0bb5
Vladyslav Havryliuk <vhavryliuk@corevalue.net>
2019-11-14 17:19:04 +0200

    connect ShoppingEffects in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
 
 import {AppComponent} from './app.component';
-import {AppEffects} from './app.effects';
 import {ShoppingCartComponent} from './components/shopping-cart/shopping-cart.component';
 import {ShoppingDialogComponent} from './components/shopping-dialog/shopping-dialog.component';
 import {ShoppingListComponent} from './components/shopping-list/shopping-list.component';
+import {ShoppingEffects} from './effects/shopping.effects';
 import {reducers} from './reducers';
 
 @NgModule({
@@ -30,7 +30,7 @@ import {reducers} from './reducers';
             }
         }),
         StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
-        EffectsModule.forRoot([AppEffects])
+        EffectsModule.forRoot([ShoppingEffects])
     ],
     providers: [],
     bootstrap: [AppComponent]
