eb2c2515aeacd86d8f11a5073ab33f6b706c901a
Vladyslav Havryliuk <vhavryliuk@corevalue.net>
2019-11-15 19:07:50 +0200

    add code logic to AppComponent class && fix its template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {AppState} from './models/app-state.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shopping-app';
+export class AppComponent implements OnInit {
+    public isOpen$: Observable<boolean>;
+    public error$: Observable<Error>;
+
+    constructor(private store: Store<AppState>) {
+    }
+
+    ngOnInit() {
+        this.isOpen$ = this.store.select(store => store.dialog.isOpen);
+        this.error$ = this.store.select(store => store.shopping.error);
+    }
 }
