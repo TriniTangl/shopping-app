6783f8408b3755d2e91072df41cdb61e2b00bcfd
Vladyslav Havryliuk <vhavryliuk@corevalue.net>
2019-11-15 19:11:34 +0200

    add code logic to ShoppingListComponent and fix its template

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {OpenDialogAddingAction, OpenDialogEditingAction} from '../../actions/dialog.actions';
+import {DeleteItemAction, EditItemAction, LoadItemsAction} from '../../actions/shopping.actions';
+import {AppState} from '../../models/app-state.model';
+import {ShoppingItem} from '../../models/shopping.model';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+    public shoppingItems: Array<ShoppingItem>;
+    public loading$: Observable<boolean>;
 
-  constructor() { }
+    constructor(private store: Store<AppState>) {
+        this.store.dispatch(new LoadItemsAction());
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.store.select(store => store.shopping.list).subscribe(list => this.shoppingItems = list);
+        this.loading$ = this.store.select(store => store.shopping.loading);
+    }
 
+    public addItem(): void {
+        this.store.dispatch(new OpenDialogAddingAction());
+    }
+
+    public editItem(item: ShoppingItem): void {
+        this.store.dispatch(new OpenDialogEditingAction(item));
+    }
+
+    public addItemToCart(item: ShoppingItem): void {
+        if (item.amount > 0) {
+            item = {
+                ...item,
+                amount: item.amount - 1,
+                amountInCart: item.amountInCart + 1
+            };
+
+            this.store.dispatch(new EditItemAction(item));
+        }
+    }
+
+    public deleteItem(id: string): void {
+        this.store.dispatch(new DeleteItemAction(id));
+    }
 }
