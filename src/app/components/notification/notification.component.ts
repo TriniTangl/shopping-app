7f1aab83130342c6f4a31119017e04ddf42ab3b7
Vladyslav Havryliuk <vhavryliuk@corevalue.net>
2019-12-13 15:49:40 +0200

    add code logic to NotificationComponent, add its template and style

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Notification, NotificationService} from '../../services/notification.service';
 
 @Component({
     selector: 'app-notification',
@@ -6,11 +7,27 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent implements OnInit {
+    public notification: Notification;
+    private timer: any;
 
-    constructor() {
+    constructor(private notificationService: NotificationService) {
+        this.notification = {
+            type: '',
+            message: ''
+        };
+        this.notificationService.notification$.subscribe(n => {
+            this.notification = n;
+            this.timer = setTimeout(() => {
+                this.closeNotification();
+            }, 3000);
+        });
     }
 
     ngOnInit() {
     }
 
+    public closeNotification() {
+        this.notification.message = '';
+        clearTimeout(this.timer);
+    }
 }
